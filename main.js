115e822c7de9789d1580ca13486d79b61df1d27a
mrsk613 <74517898+mrsk613@users.noreply.github.com>
2022-02-07 14:34:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ footerArea.innerHTML = "Copyright © 2020 Roly-Poly All Rights Reserved.";
 
 function fadeAnime() {
     $('.fadeInTrigger').each(function () { //fadeInTriggerというクラス名が
-        var elemPos = $(this).offset().top - 50;//要素より、50px上の
+        var elemPos = $(this).offset().top - 10;//要素より、50px上の
         var scroll = $(window).scrollTop();
         var windowHeight = $(window).height();
         if (scroll >= elemPos - windowHeight) {
@@ -25,6 +25,57 @@ function fadeAnime() {
     })
 };
 
-$(window).scroll(function (){
+$(window).scroll(function () {
     fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });
+});
+
+$('.slider').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,//次のスライドに切り替わる待ち時間
+    speed: 1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function (event, slick, currentSlide, nextSlide) {
+    $('.slider').slick('slickPlay');
+});
+
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
